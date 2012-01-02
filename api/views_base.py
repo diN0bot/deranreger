11e723c2f2b14ad8b77f1fcf0882d1b283869767
lucy <lucy.mendel@rackspace.com>
2012-01-01 21:23:47 -0800

    barest bones of a structure smasher idea

--- api/views_base.py
+++ api/views_base.py
@@ -3,6 +3,7 @@ from django.utils import simplejson as json
 from django.views.generic.base import View
 from django.views.generic.edit import FormMixin
 import pyes
+from time import mktime
 from django.conf import settings
 
 
@@ -10,6 +11,13 @@ __all__ = ["BaseJSONView"]
 
 
 class JSONResponseMixin(object):
+
+    def __init__(self):
+        super(JSONResponseMixin, self).__init__()
+        self.structure_definitions = {'dict': self._smash,
+                                      'list': self._smash,
+                                      'datetime': lambda dt: int(mktime(dt.timetuple())) }
+
     def render_to_response(self, context):
         "Returns a JSON response containing 'context' as payload"
         return self.get_json_response(self.convert_context_to_json(context))
@@ -22,11 +30,34 @@ class JSONResponseMixin(object):
 
     def convert_context_to_json(self, context):
         "Convert the context dictionary into a JSON object"
-        # Note: This is *EXTREMELY* naive; in reality, you'll need
-        # to do much more complex handling to ensure that arbitrary
-        # objects -- such as Django model instances or querysets
-        # -- can be serialized as JSON.
+        context = self._smash(context)
         return json.dumps(context)
+
+    def _smashit(self, context):
+        try:
+            return self.structure_definitions[context.__class__.__name__](context)
+        except KeyError:
+            return context
+
+    def _smash(self, context):
+        if hasattr(context, 'iteritems'):
+            return self._dict_smash(context)
+        elif hasattr(context, 'append'):
+            return self._list_smash(context)
+        else:
+            return self._smashit(context)
+
+    def _dict_smash(self, adict):
+        ret = {}
+        for key, value in adict.iteritems():
+            ret[key] = self._smashit(value)
+        return ret
+
+    def _list_smash(self, alist):
+        newlist = []
+        for value in alist:
+            newlist.append(self._smashit(value))
+        return newlist
 
 
 class ElasticSearchMixin(object):
@@ -46,6 +77,9 @@ class ElasticSearchMixin(object):
 
 
 class BaseJSONView(FormMixin, View, JSONResponseMixin, ElasticSearchMixin):
+
+    def __init__(self):
+        JSONResponseMixin.__init__(self)
 
     def form_valid(self, form, request):
         #response = self._es_conn.index(form.document())
